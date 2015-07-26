72cfe515e1e57cd6f771b9bcee9a1153e1f75e64
appcamp <appcamp@appcamps-MacBook-Pro-4.local>
2015-07-26 15:18:12 +0200

    no message

--- RockPaperScissors WatchKit Extension/NotificationController.swift
+++ RockPaperScissors WatchKit Extension/NotificationController.swift
@@ -30,15 +30,21 @@ class NotificationController: WKUserNotificationInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-/*
+
     override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
         // This method is called when a local notification needs to be presented.
         // Implement it if you use a dynamic notification interface.
         // Populate your dynamic notification interface as quickly as possible.
         //
         // After populating your dynamic notification interface call the completion block.
-        i        completionHandler(.Custom)
-    }*/
+        alertLabel.setText(localNotification.alertBody)
+        if let userInfo = localNotification.userInfo as? [String: Int] {
+            if let highscore = userInfo["highscore"] {
+                highscoreLabel.setText("Last highscore: \(highscore)")
+            }
+        }
+        completionHandler(.Custom)
+    }
     
     
     override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
